121c39dbb6f1ac962266d5f9596d7d9a8c6b3c94
Christopher Bartling <chris.bartling@gmail.com>
2021-05-15 11:46:19 -0500

    Setting up initial aliases

---
+++ aliases/core.sh
@@ -0,0 +1,18 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+
+alias zshconfig="code ~/.zshrc"
+alias aliases-config="code ~/.dotfiles/aliases/core.sh"
+alias ohmyzsh="code ~/.oh-my-zsh"
+alias java8="sdk use java 8.0.232.hs-adpt && java -version"
+alias java11="sdk use java 11.0.6.hs-adpt && java -version"
+alias java12="sdk use java 12.0.1.hs-adpt && java -version"
+alias java13="sdk use java 13.0.2.hs-adpt && java -version"
+alias java15="sdk use java 15.0.2.hs-adpt && java -version"
+alias nuget="mono /usr/local/bin/nuget.exe"
+alias yarn-up="yarn upgrade-interactive --latest"
+
+alias cebartling-github-cd="cd ~/git-sandbox/cebartling"
+
