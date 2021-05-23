ef797f9414b9192c064ca895b0fbd175afb78c63
Christopher Bartling <chris.bartling@gmail.com>
2021-05-23 06:35:33 -0500

    More work on aliases

---
+++ aliases/studying.sh
@@ -0,0 +1,3 @@
+alias study-guides-and-cheatsheets-cd="cd ~/github-sandbox/cebartling/study-guides-and-cheatsheets"
+alias study-books-cd="cd ~/github-sandbox/cebartling/study-books"
+alias aws-certified-solution-architect-associate-2020-cd="cd ~/github-sandbox/cebartling/aws-certified-solution-architect-associate-2020"
