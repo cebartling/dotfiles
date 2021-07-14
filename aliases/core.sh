c6eb5b5b73f7cc4c5ce4cf85d6e7bba65d7cd3b7
Christopher Bartling <chris.bartling@gmail.com>
2021-07-14 11:04:34 -0500

    Reformatting and comments

--- aliases/core.sh
+++ aliases/core.sh
@@ -3,6 +3,8 @@
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
 
+# Common aliases
+alias weather="curl wttr.in/55379"
 alias zshconfig="code ~/.zshrc"
 alias aliases-config="code ~/.dotfiles/aliases/core.sh"
 alias ohmyzsh="code ~/.oh-my-zsh"
@@ -14,11 +16,10 @@ alias java15="sdk use java 15.0.2.hs-adpt && java -version"
 alias nuget="mono /usr/local/bin/nuget.exe"
 alias yarn-up="yarn upgrade-interactive --latest"
 
+# Specific alias loaders
 alias cebartling-github-cd="cd ~/github-sandbox/cebartling"
 alias gummi-bear-aliases="source ~/.dotfiles/aliases/gummi-bears.sh"
 alias quizzer-aliases="source ~/.dotfiles/aliases/quizzer.sh"
 alias saga-pattern-aliases="source ~/.dotfiles/aliases/saga-pattern.sh"
 alias swift-aliases="source ~/.dotfiles/aliases/swift.sh"
 alias studying-aliases="source ~/.dotfiles/aliases/studying.sh"
-
-alias weather="curl wttr.in/55379"
