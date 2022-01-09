a7c2756574a78dc131e2915903edb6b9c7fb3a42
Christopher Bartling <chris.bartling@gmail.com>
2022-01-09 14:10:48 -0600

    Aliases for test-driven repository

--- aliases/test-driven.sh
+++ aliases/test-driven.sh
@@ -1,1 +1,6 @@
+echo "Configuring test-driven aliases..."
+
 alias test-driven-cd="cd ~/github-sandbox/cebartling/test-driven"
+alias test-driven-spring-boot-webflux-tdd-demo-cd="cd ~/github-sandbox/cebartling/test-driven/spring-boot/webflux-tdd-demo && pwd"
+alias test-driven-testcontainers-java-demo-cd="cd ~/github-sandbox/cebartling/test-driven/testcontainers/java-demo && pwd"
+alias test-driven-svelte-example1-cd="cd ~/github-sandbox/cebartling/test-driven/svelte/example1 && pwd"
