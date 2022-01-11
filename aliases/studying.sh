b52d1ac4178a406df719eaa71717cf9a86ccada3
Christopher Bartling <chris.bartling@gmail.com>
2022-01-11 14:11:49 -0600

    More studying aliases

--- aliases/studying.sh
+++ aliases/studying.sh
@@ -4,7 +4,8 @@ alias study-guides-and-cheatsheets-cd="cd ~/github-sandbox/cebartling/study-guid
 
 alias study-books-cd="cd ~/github-sandbox/cebartling/study-books"
 alias study-books-build="cd ~/github-sandbox/cebartling/study-books && mdbook build"
-alias study-books-serve="cd ~/github-sandbox/cebartling/study-books && mdbook serve && open http://localhost:3000"
+alias study-books-serve="cd ~/github-sandbox/cebartling/study-books && mdbook serve"
+alias study-books-open-browser="open http://localhost:3000"
 
 alias aws-certified-solution-architect-associate-2020-cd="cd ~/github-sandbox/cebartling/aws-certified-solution-architect-associate-2020"
 
@@ -14,5 +15,6 @@ echo "study-guides-and-cheatsheets-cd"
 echo "study-books-cd"
 echo "study-books-build"
 echo "study-books-serve"
+echo "study-books-open-browser"
 echo "aws-certified-solution-architect-associate-2020-cd"
 echo "-----------------------------------------------------"
