0462a04073c592cdea19f1569b1ab5b5480c918d
Christopher Bartling <chris.bartling@gmail.com>
2022-02-13 11:03:06 -0600

    Adding aliases for test-driven repository

--- aliases/test-driven.sh
+++ aliases/test-driven.sh
@@ -3,15 +3,23 @@ echo "Configuring test-driven aliases..."
 alias test-driven-cd="cd ~/github-sandbox/cebartling/test-driven"
 alias test-driven-spring-boot-webflux-tdd-demo-cd="cd ~/github-sandbox/cebartling/test-driven/spring-boot/webflux-tdd-demo && pwd && java11"
 alias test-driven-testcontainers-java-demo-cd="cd ~/github-sandbox/cebartling/test-driven/testcontainers/java-demo && pwd && java11"
+
+alias test-driven-angular-example1-cd="cd ~/github-sandbox/cebartling/test-driven/angular/example1 && pwd && nvm use"
 alias test-driven-svelte-example1-cd="cd ~/github-sandbox/cebartling/test-driven/svelte/example1 && pwd && nvm use"
+alias test-driven-react-example1-cd="cd ~/github-sandbox/cebartling/test-driven/react/example1 && pwd && nvm use"
+
 alias test-driven-storybook-angular-cd="cd ~/github-sandbox/cebartling/test-driven/storybook/angular/storybook-example && pwd && nvm use"
+alias test-driven-storybook-svelte-cd="cd ~/github-sandbox/cebartling/test-driven/storybook/svelte/storybook-example && pwd && nvm use"
 
 echo "Available aliases"
 echo "---------------------------------------------"
 echo "test-driven-cd"
 echo "test-driven-spring-boot-webflux-tdd-demo-cd"
 echo "test-driven-testcontainers-java-demo-cd"
+echo "test-driven-angular-example1-cd"
 echo "test-driven-svelte-example1-cd"
+echo "test-driven-react-example1-cd"
 echo "test-driven-storybook-angular-cd"
+echo "test-driven-storybook-svelte-cd"
 echo "---------------------------------------------"
 
