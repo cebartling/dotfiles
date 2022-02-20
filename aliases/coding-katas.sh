4e2e428f9c54cc16a994e600585858b06b55c90f
Christopher Bartling <chris.bartling@gmail.com>
2022-02-20 14:14:34 -0600

    Adding aliases for coding katas

--- aliases/coding-katas.sh
+++ aliases/coding-katas.sh
@@ -1,9 +1,13 @@
 echo "Configuring code katas aliases..."
 
 alias coding-katas-cd="cd ~/github-sandbox/cebartling/coding-katas"
+alias coding-katas-python-cd="cd ~/github-sandbox/cebartling/coding-katas/python/katas && pwd && poetry shell"
+alias coding-katas-rust-cd="cd ~/github-sandbox/cebartling/coding-katas/rust/katas && pwd"
 
 echo "Available aliases"
 echo "---------------------------------------------"
 echo "coding-katas-cd"
+echo "coding-katas-python-cd"
+echo "coding-katas-rust-cd"
 echo "---------------------------------------------"
 
