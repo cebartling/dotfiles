8bfc7f8fc4df5c6cb7b5c89c59b1b8ad23fd02a8
Christopher Bartling <chris.bartling@gmail.com>
2022-02-21 11:47:10 -0600

    Adding Ubuntu tools install script

---
+++ scripts/ubuntu_install_tools.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo "Installing essentials tools on Ubuntu..."
+
+sudo apt install bat
+sudo apt install exa
+sudo apt install fd-find
+sudo apt install ripgrep
+sudo snap install httpie
+sudo snap install procs
+
+echo "Finished installing essentials tools on Ubuntu!"
