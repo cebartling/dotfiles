e1622c649de0772890581355354309fe02ee7d0b
Christopher Bartling <chris.bartling@gmail.com>
2022-02-21 11:58:47 -0600

    Installation scripts for macOS and Ubuntu

---
+++ scripts/macOS/install_tools.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo "Installing essentials tools on macOS..."
+
+brew install bat
+brew install exa
+brew install ripgrep
+brew install httpie
+brew install procs
+brew install dog
+
+echo "Finished installing essentials tools on macOS!"
