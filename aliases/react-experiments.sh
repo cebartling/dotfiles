94aed6c523ba839704ddf0baa569882cbe0025ca
Christopher Bartling <chris.bartling@gmail.com>
2022-02-21 10:14:44 -0600

    Adding aliases for React experiments

---
+++ aliases/react-experiments.sh
@@ -0,0 +1,9 @@
+echo "Configuring React experiments aliases..."
+
+alias react-experiments-cd="cd ~/github-sandbox/cebartling/react-experiments"
+
+echo "Available aliases"
+echo "---------------------------------------------"
+echo "react-experiments-cd"
+echo "---------------------------------------------"
+
