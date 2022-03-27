e423c77af44d45c2253b8d35c8eb201baf5f1855
Christopher Bartling <chris.bartling@gmail.com>
2022-03-27 00:44:13 -0500

    Fix issue with extra &&

--- aliases/testcontainers-experiments.sh
+++ aliases/testcontainers-experiments.sh
@@ -1,7 +1,7 @@
 echo "Configuring testcontainers experiments aliases..."
 
 alias testcontainers-experiments-cd="cd ~/github-sandbox/cebartling/testcontainers-experiments"
-alias testcontainers-experiments-kotlin-demo-cd="cd ~/github-sandbox/cebartling/testcontainers-experiments/kotlin/demo && pwd && sdk use java 17.0.2-tem && && java -version"
+alias testcontainers-experiments-kotlin-demo-cd="cd ~/github-sandbox/cebartling/testcontainers-experiments/kotlin/demo && pwd && sdk use java 17.0.2-tem && java -version"
 
 echo "Available aliases"
 echo "------------------------------------------------------"
