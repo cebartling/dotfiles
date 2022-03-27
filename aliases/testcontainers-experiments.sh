9f2ad67f152012f6f91ab1096375a6c1330dc329
Christopher Bartling <chris.bartling@gmail.com>
2022-03-27 00:42:23 -0500

    Adding aliases for testcontainers-experiments

---
+++ aliases/testcontainers-experiments.sh
@@ -0,0 +1,10 @@
+echo "Configuring testcontainers experiments aliases..."
+
+alias testcontainers-experiments-cd="cd ~/github-sandbox/cebartling/testcontainers-experiments"
+alias testcontainers-experiments-kotlin-demo-cd="cd ~/github-sandbox/cebartling/testcontainers-experiments/kotlin/demo && pwd && sdk use java 17.0.2-tem && && java -version"
+
+echo "Available aliases"
+echo "------------------------------------------------------"
+echo "testcontainers-experiments-cd"
+echo "testcontainers-experiments-kotlin-demo-cd"
+echo "------------------------------------------------------"
