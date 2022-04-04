243f15a3e9e2005145aa106ac5622db12e51981d
Christopher Bartling <chris.bartling@gmail.com>
2022-04-04 15:00:35 -0500

    Adding aliases for Remix experiments

---
+++ aliases/remix-experiments.sh
@@ -0,0 +1,11 @@
+echo "Configuring Remix experiments aliases..."
+
+alias remix-experiments-cd="cd ~/github-sandbox/cebartling/remix-experiments"
+alias remix-experiments-example1-cd="cd ~/github-sandbox/cebartling/remix-experiments/example1 && pwd && nvm use"
+
+echo "Available aliases"
+echo "---------------------------------------------"
+echo "remix-experiments-cd"
+echo "remix-experiments-example1-cd"
+echo "---------------------------------------------"
+
