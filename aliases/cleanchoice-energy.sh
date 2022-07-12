ced4fd69ed99c1f29a4847ad378bfe7197f0fbfe
Chris Bartling <chris.bartling@cleanchoiceenergy.com>
2022-07-11 22:15:31 -0500

    Adding alias for API project

--- aliases/cleanchoice-energy.sh
+++ aliases/cleanchoice-energy.sh
@@ -5,12 +5,14 @@ autoload colors; colors
 echo $fg[cyan]Configuring CleanChoice Energy CEO aliases...$reset_color
 
 
+alias cce-ceo-api-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option-API && pwd && nvm use"
 alias cce-ceo-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option && pwd && nvm use"
 alias cce-ceo-web-ui-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option/web-ui && pwd && nvm use"
 
 
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
+echo $fg[yellow]cce-ceo-api-cd$reset_color
 echo $fg[yellow]cce-ceo-cd$reset_color
 echo $fg[yellow]cce-ceo-web-ui-cd$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
