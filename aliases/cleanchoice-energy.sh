bd187c20c374112e203d9711df323ed3b7872408
Chris Bartling <chris.bartling@cleanchoiceenergy.com>
2022-07-11 22:18:56 -0500

    Alias for loading personal GitHub identity

--- aliases/cleanchoice-energy.sh
+++ aliases/cleanchoice-energy.sh
@@ -8,6 +8,7 @@ echo $fg[cyan]Configuring CleanChoice Energy CEO aliases...$reset_color
 alias cce-ceo-api-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option-API && pwd && nvm use"
 alias cce-ceo-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option && pwd && nvm use"
 alias cce-ceo-web-ui-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option/web-ui && pwd && nvm use"
+alias personal-github-identity="eval \"$(ssh-agent -s)\" && ssh-add ~/.ssh/id_ed25519_personal"
 
 
 echo $fg[cyan]Available aliases$reset_color
