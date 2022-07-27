cb2e6d6c23dac49b9353b1fc27a980c4b430f3df
Christopher Bartling <chris.bartling@gmail.com>
2022-07-27 11:12:06 -0500

    feat: adding CleanChoice Energy aliases

--- aliases/cleanchoice-energy.sh
+++ aliases/cleanchoice-energy.sh
@@ -7,14 +7,18 @@ echo $fg[cyan]Configuring CleanChoice Energy CEO aliases...$reset_color
 
 alias cce-ceo-api-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option-API && pwd && nvm use"
 alias cce-ceo-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option && pwd && nvm use"
+alias cce-cd="cd ~/github-sandbox/cleanchoice && pwd"
+alias cce-europa-cd="cd ~/github-sandbox/cleanchoice/europa && pwd"
 alias cce-ceo-web-ui-cd="cd ~/github-sandbox/cleanchoice/clean-energy-option/web-ui && pwd && nvm use"
 alias personal-github-identity="eval \"$(ssh-agent -s)\" && ssh-add ~/.ssh/id_ed25519_personal"
 
 
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
-echo $fg[yellow]cce-ceo-api-cd$reset_color
+echo $fg[yellow]cce-cd$reset_color
+echo $fg[yellow]cce-europa-cd$reset_color
 echo $fg[yellow]cce-ceo-cd$reset_color
+echo $fg[yellow]cce-ceo-api-cd$reset_color
 echo $fg[yellow]cce-ceo-web-ui-cd$reset_color
 echo $fg[yellow]personal-github-identity$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
