7a75434805167ada375770b221bf0f391a599fb1
Christopher Bartling <chris.bartling@gmail.com>
2023-02-17 21:38:59 -0600

    feat: nvm via homebrew runtime script

---
+++ runtimes/nvm-homebrew.sh
@@ -0,0 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+
+# This loads nvm
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  
+
+# This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  
