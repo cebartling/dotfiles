576155b0f3a7ff6425613f4744bbd32eec6692f2
Christopher Bartling <chris.bartling@gmail.com>
2023-04-09 19:21:03 -0500

    fix: pyenv configuration

--- aliases/localstack-experiments.sh
+++ aliases/localstack-experiments.sh
@@ -2,10 +2,14 @@
 
 autoload colors; colors
 
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 echo $fg[cyan]Configuring LocalStack experiments aliases...$reset_color
 
 
-alias localstack-experiments-cd="cd ~/github-sandbox/cebartling/localstack-experiments && eval \"$(pyenv init -)\" && eval \"$(pyenv virtualenv-init -)\" && pwd"
+alias localstack-experiments-cd="cd ~/github-sandbox/cebartling/localstack-experiments && pwd"
+
 
 
 echo $fg[cyan]Available aliases$reset_color
