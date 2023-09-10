eda300166da5b8d7cab488e211c045df1c0ff4dc
Christopher Bartling <chris.bartling@gmail.com>
2023-09-09 20:24:09 -0500

    feat: adding alias

--- aliases/aws-cdk-experiments.sh
+++ aliases/aws-cdk-experiments.sh
@@ -9,10 +9,11 @@ echo $fg[cyan]Configuring AWS CDK experiments aliases...$reset_color
 
 
 alias aws-cdk-experiments-cd="cd ~/github-sandbox/cebartling/aws-cdk-experiments && pwd"
-
+alias install-aws-cdk="npm install -g aws-cdk"
 
 
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
 echo $fg[yellow]aws-cdk-experiments-cd$reset_color
+echo $fg[yellow]install-aws-cdk$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
