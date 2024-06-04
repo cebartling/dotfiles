fe918269d813bf619e2b2f61e1415452bdf1634f
Christopher Bartling <chris.bartling@gmail.com>
2024-06-04 14:32:39 -0500

    fix: fixing the SSH key name

--- aliases/costco.sh
+++ aliases/costco.sh
@@ -5,7 +5,7 @@ autoload colors; colors
 echo $fg[cyan]Configuring Costco OMS Modernization engagement aliases...$reset_color
 
 alias costco-oms-ui-cd="cd ~/github-sandbox/costco/oms-ui && nvm use && pwd && costco-add-ssh-key && costco-git-config"
-alias costco-add-ssh-key="ssh-add ~/.ssh/id_rsa_costco && ssh-add -l"
+alias costco-add-ssh-key="ssh-add ~/.ssh/costco-rsa-key && ssh-add -l"
 alias costco-git-config="git config user.name && git config user.email"
 
 
