e9f1f69172b38dd34016104f4e70a9bb12328891
Christopher Bartling <chris.bartling@gmail.com>
2024-06-04 17:23:23 -0500

    feat: adding new alias

--- aliases/costco.sh
+++ aliases/costco.sh
@@ -5,6 +5,7 @@ autoload colors; colors
 echo $fg[cyan]Configuring Costco OMS Modernization engagement aliases...$reset_color
 
 alias costco-oms-ui-cd="cd ~/github-sandbox/costco/oms-ui && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+alias costco-oms-domain-modeling-cd="cd ~/github-sandbox/costco/oms-domain-modeling && pwd && costco-add-ssh-key && costco-git-config"
 alias costco-add-ssh-key="ssh-add ~/.ssh/costco-rsa-key && ssh-add -l"
 alias costco-git-config="git config user.name && git config user.email"
 
@@ -12,6 +13,7 @@ alias costco-git-config="git config user.name && git config user.email"
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
 echo $fg[yellow]costco-oms-ui-cd$reset_color
+echo $fg[yellow]costco-oms-domain-modeling-cd$reset_color
 echo $fg[yellow]costco-add-ssh-key$reset_color
 echo $fg[yellow]costco-git-config$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
