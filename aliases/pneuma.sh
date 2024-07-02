f9b91ab6becb15fceec3cae7362ad66d2381035d
Christopher Bartling <chris.bartling@gmail.com>
2024-07-02 10:17:58 -0500

    feat: adding pneuma aliases

---
+++ aliases/pneuma.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+autoload colors; colors
+
+echo $fg[cyan]Configuring pneuma aliases...$reset_color
+
+alias pneuma-monorepo-cd="cd ~/github-sandbox/cebartling/pneuma && sdk use && pwd && costco-git-config"
+
+echo ""
+echo $fg[cyan]Available aliases$reset_color
+echo $fg[cyan]---------------------------------------------------$reset_color
+echo $fg[yellow]pneuma-monorepo-cd$reset_color
+echo $fg[cyan]---------------------------------------------------$reset_color
