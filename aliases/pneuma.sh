25c604c996b837803d05ca5af07380e634439549
Christopher Bartling <chris.bartling@gmail.com>
2024-07-04 20:25:11 -0500

    feat: adding pneuma aliases

--- aliases/pneuma.sh
+++ aliases/pneuma.sh
@@ -5,9 +5,11 @@ autoload colors; colors
 echo $fg[cyan]Configuring pneuma aliases...$reset_color
 
 alias pneuma-monorepo-cd="cd ~/github-sandbox/cebartling/pneuma && sdk use java 21.0.3-tem && pwd && git-config"
+alias pneuma-app-cd="cd ~/github-sandbox/cebartling/pneuma/apps/pneuma && sdk use java 21.0.3-tem && pwd && git-config"
 
 echo ""
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
 echo $fg[yellow]pneuma-monorepo-cd$reset_color
+echo $fg[yellow]pneuma-app-cd$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
