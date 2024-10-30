c22ae7195350b5a46cabac70d702c6b697ad2af9
Christopher Bartling <chris.bartling@gmail.com>
2024-10-30 12:03:53 -0500

    feat: adding new aliases for Azure dev

---
+++ aliases/azure-experiments.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+autoload colors
+colors
+
+echo $fg[cyan]Configuring Azure experiments aliases...$reset_color
+
+alias azure-experiments-cd="cd ~/github-sandbox/cebartling/azure && nvm use && pwd"
+
+echo $fg[cyan]Available aliases$reset_color
+echo $fg[cyan]---------------------------------------------$reset_color
+echo $fg[yellow]azure-experiments-cd$reset_color
+echo $fg[cyan]---------------------------------------------$reset_color
