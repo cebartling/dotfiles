aed78795698747bcbf69f6de789e97b3d34f16af
Christopher Bartling <chris.bartling@gmail.com>
2024-11-24 21:19:07 -0600

    feat: updating config

--- templates/eslint/eslint.config.mjs
+++ templates/eslint/eslint.config.mjs
@@ -2,9 +2,26 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
 // https://typescript-eslint.io/packages/typescript-eslint#config
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended
-);
+export default tseslint.config({
+  languageOptions: {
+    parserOptions: {
+      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  // Set the react version
+  settings: { react: { version: "18.3" } },
+  plugins: {
+    // Add the react plugin
+    react,
+  },
+  rules: {
+    // other rules...
+    // Enable its recommended rules
+    ...react.configs.recommended.rules,
+    ...react.configs["jsx-runtime"].rules,
+  },
+});
