47e2ccf369b89db011aaf3963174e0b6325f3e00
Christopher Bartling <chris.bartling@gmail.com>
2024-12-18 19:07:32 -0600

    feat: tweaking alias

--- aliases/replay-forensics.sh
+++ aliases/replay-forensics.sh
@@ -6,7 +6,7 @@ colors
 echo $fg[cyan]Configuring Replay Forensics aliases...$reset_color
 
 alias rfi-github-cd="cd ~/github-sandbox/ReplayForensics && pwd && rfi-add-ssh-key && rfi-git-config"
-alias rfi-frontend-app-cd="cd ~/github-sandbox/ReplayForensics/frontend-app && pwd && rfi-add-ssh-key && rfi-git-config"
+alias rfi-frontend-app-cd="cd ~/github-sandbox/ReplayForensics/frontend-app && pwd && rfi-add-ssh-key && rfi-git-config && nvm use"
 alias rfi-add-ssh-key="ssh-add -D && ssh-add ~/.ssh/id_ed25519_rfi && ssh-add -l"
 alias rfi-git-config="git config user.name \"Christopher Bartling\" && git config user.email \"chris@replayforensics.com\""
 
