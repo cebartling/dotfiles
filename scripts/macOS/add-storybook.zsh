cd2fb49e6d727f71217ce611f2da8c413159700f
Christopher Bartling <chris.bartling@gmail.com>
2024-12-29 22:10:55 -0600

    chore: updating script

--- scripts/macOS/add-storybook.zsh
+++ scripts/macOS/add-storybook.zsh
@@ -8,8 +8,18 @@ echo $fg[cyan]Adding Storybook and addons to current React project...$reset_colo
 npx storybook@latest init --no-dev
 npm install -D @storybook/addon-essentials
 npm install -D @storybook/test-runner jest @storybook/react-vite @vitejs/plugin-react @babel/preset-env @babel/preset-typescript @babel/preset-react npx install @chromatic-com/storybook
-npm install -D @storybook/addon-measure @storybook/addon-outline @storybook/addon-actions @storybook/addon-actions @storybook/addon-links @storybook/addon-a11y @storybook/addon-controls @storybook/addon-viewport @storybook/addon-backgrounds @storybook/addon-toolbars @storybook/addon-knobs
-npm install -D storybook-addon-performance
+npm install -D @storybook/addon-measure
+npm install -D @storybook/addon-outline
+npm install -D @storybook/addon-actions
+npm install -D @storybook/addon-links
+npm install -D @storybook/addon-a11y
+npm install -D @storybook/addon-controls
+npm install -D @storybook/addon-viewport
+npm install -D @storybook/addon-backgrounds
+npm install -D @storybook/addon-toolbars
+#npm install -D @storybook/addon-knobs
+#npm install -D storybook-addon-performance
+npm install -D msw-storybook-addon@latest
 
 # Add MSW to Storybook
 echo $fg[yellow]Installing and configuring Mock Service Worker...$reset_color
