24b4bdcc93f28c2641f74ad0d9e9d9be82841dfc
Christopher Bartling <chris.bartling@gmail.com>
2025-03-08 22:35:19 -0600

    feat: adding font-monaspace

--- scripts/macOS/install_fonts_via_homebrew.zsh
+++ scripts/macOS/install_fonts_via_homebrew.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
-echo "Installing Nerd Fonts..."
+echo "Installing fonts..."
 
 brew install --cask font-3270-nerd-font
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-liberation-nerd-font
 brew install --cask font-hack-nerd-font
+brew install --cask font-monaspace
 
-echo "Finished installing Nerd Fonts!"
+echo "Finished installing fonts!"
