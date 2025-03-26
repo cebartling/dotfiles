e3a4b106790adcee7d8ff6b5962e13e5168b434f
Christopher Bartling <chris.bartling@gmail.com>
2025-03-26 12:04:46 -0500

    feat: adding new aliases

--- aliases/costco-browse.sh
+++ aliases/costco-browse.sh
@@ -5,7 +5,7 @@ colors
 
 echo $fg[cyan]Configuring Costco Digital Browse Modernization aliases...$reset_color
 
-alias costco-browse-cd="cd ~/github-sandbox/costco/browse.costco.web && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+#alias costco-browse-cd="cd ~/github-sandbox/costco/browse.costco.web && nvm use && pwd && costco-add-ssh-key && costco-git-config"
 alias costco-digital-forge-cd="cd ~/github-sandbox/costco/forge.digital.components && nvm use && pwd && costco-add-ssh-key && costco-git-config"
 alias costco-public-forge-cd="cd ~/github-sandbox/costco/forge.public.components && nvm use && pwd && costco-add-ssh-key && costco-git-config"
 alias costco-public-forge-lint-cd="cd ~/github-sandbox/costco/forge.public.lint && nvm use && pwd && costco-add-ssh-key && costco-git-config"
@@ -14,6 +14,13 @@ alias costco-consent-ui-cd="cd ~/github-sandbox/costco/all.costco.web.consent-ma
 alias costco-consent-web-cd="cd ~/github-sandbox/costco/consent.costco.web && nvm use && pwd && costco-add-ssh-key && costco-git-config"
 alias costco-add-ssh-key="ssh-add -D && ssh-add ~/.ssh/costco-rsa-key && ssh-add -l"
 alias costco-git-config="git config user.name \"Christopher Bartling\" && git config user.email \"c_cbartling@costco.com\""
+
+alias costco-gdx-forge-digital-cd="cd ~/github-sandbox/costco/gdx-lib-forge-digital-components && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+alias costco-gdx-forge-public-cd="cd ~/github-sandbox/costco/gdx-lib-forge-public-components && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+alias costco-gdx-forge-public-icons-cd="cd ~/github-sandbox/costco/gdx-lib-forge-public-icons && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+alias costco-gdx-forge-public-tokens-cd="cd ~/github-sandbox/costco/gdx-lib-forge-public-tokens && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+alias costco-gdx-browse-homepage-cd="cd ~/github-sandbox/costco/gdx-ux-cnsw-browse-homepage && nvm use && pwd && costco-add-ssh-key && costco-git-config"
+alias costco-gdx-browse-search-cd="cd ~/github-sandbox/costco/gdx-ux-cnsw-browse-search && nvm use && pwd && costco-add-ssh-key && costco-git-config"
 
 alias costco-start-redis="docker remove costco-browse-redis-stack && docker run -d --name costco-browse-redis-stack -p 6379:6379  redis/redis-stack-server:latest"
 alias costco-start-redis-insight="docker remove costco-browse-redis-insight && docker run -d --name costco-browse-redis-insight -p 5540:5540 redis/redisinsight:latest"
@@ -29,7 +36,7 @@ alias costco-run-tests="npm test --max-old-space-size=8192"
 
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
-echo $fg[yellow]costco-browse-cd$reset_color
+#echo $fg[yellow]costco-browse-cd$reset_color
 echo $fg[yellow]costco-digital-forge-cd$reset_color
 echo $fg[yellow]costco-public-forge-cd$reset_color
 echo $fg[yellow]costco-search-cd$reset_color
@@ -38,6 +45,13 @@ echo $fg[yellow]costco-consent-web-cd$reset_color
 echo $fg[yellow]costco-public-forge-lint-cd$reset_color
 echo $fg[yellow]costco-add-ssh-key$reset_color
 echo $fg[yellow]costco-git-config$reset_color
+echo ""
+echo $fg[yellow]costco-gdx-forge-digital-cd$reset_color
+echo $fg[yellow]costco-gdx-forge-public-cd$reset_color
+echo $fg[yellow]costco-gdx-forge-public-icons-cd$reset_color
+echo $fg[yellow]costco-gdx-forge-public-tokens-cd$reset_color
+echo $fg[yellow]costco-gdx-browse-homepage-cd$reset_color
+echo $fg[yellow]costco-gdx-browse-search-cd$reset_color
 echo ""
 echo $fg[yellow]costco-start-redis$reset_color
 echo $fg[yellow]costco-stop-redis$reset_color
