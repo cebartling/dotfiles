6dabd37aed71953ec52ebca6724a20c3df84b34f
Chris Bartling <e9004590@192-168-4-128.newulmtel.net>
2025-06-10 11:12:21 -0500

    feat: updating aliases

--- aliases/lifetime.sh
+++ aliases/lifetime.sh
@@ -6,12 +6,14 @@ colors
 echo $fg[cyan]Configuring Life Time, Inc. aliases...$reset_color
 
 alias lt-home-cd="cd ~/github-sandbox/lifetime && pwd && lt-add-ssh-key && lt-git-config"
+alias lt-spring-boot-splunk-spike-cd="cd ~/github-sandbox/lifetime/spring-boot-splunk-spike && pwd && lt-add-ssh-key && lt-git-config"
 alias lt-add-ssh-key="ssh-add -D && ssh-add ~/.ssh/id_ed25519_life_time && ssh-add -l"
 alias lt-git-config="git config user.name \"Christopher Bartling\" && git config user.email \"cbartling@lt.life\""
 
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
 echo $fg[yellow]lt-home-cd$reset_color
+echo $fg[yellow]lt-spring-boot-splunk-spike-cd$reset_color
 echo ""
 echo $fg[yellow]lt-add-ssh-key$reset_color
 echo $fg[yellow]lt-git-config$reset_color
