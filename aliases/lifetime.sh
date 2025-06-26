33c98cb2d387f407bdddff1dc5240f4cc9f675c0
Christopher Bartling <chris.bartling@gmail.com>
2025-06-26 08:30:09 -0500

    feat: adding expiration time limit and fixing alias issue

--- aliases/lifetime.sh
+++ aliases/lifetime.sh
@@ -8,10 +8,10 @@ echo $fg[cyan]Configuring Life Time, Inc. aliases...$reset_color
 alias lt-github-home-cd="cd ~/github-sandbox/lifetime && pwd && lt-git-setup"
 alias lt-splunkspike-cd="cd ~/github-sandbox/lifetime/splunkspike && pwd && lt-git-setup"
 alias lt-ops-confluent-kafka-cd="cd ~/github-sandbox/lifetime/ops-confluent-kafka && pwd && lt-git-setup"
-alias lt-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/credit-card-account-consumer pwd && lt-git-setup"
+alias lt-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/credit-card-account-consumer && pwd && lt-git-setup"
 
 
-alias lt-add-ssh-key="ssh-add -D && ssh-add ~/.ssh/id_ed25519_life_time && ssh-add -l"
+alias lt-add-ssh-key="ssh-add -D && ssh-add -t 8h ~/.ssh/id_ed25519_life_time && ssh-add -l"
 alias lt-git-config="git config user.name \"Christopher Bartling\" && git config user.email \"cbartling@lt.life\""
 alias lt-git-setup="lt-add-ssh-key && lt-git-config"
 
