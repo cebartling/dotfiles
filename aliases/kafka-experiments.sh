fb7c6b46624675e9e0628e6bea80a3a0972e55ac
Christopher Bartling <chris.bartling@gmail.com>
2025-07-01 22:54:04 -0500

    fix: fixing aliases

--- aliases/kafka-experiments.sh
+++ aliases/kafka-experiments.sh
@@ -9,17 +9,17 @@ alias git-set-ssh-key="ssh-add -D && ssh-add -t 24h ~/.ssh/id_ed25519 && ssh-add
 alias git-set-config="git config user.name \"Christopher Bartling\" && git config user.email \"chris.bartling@gmail.com\""
 
 alias kafka-experiments-cd="cd ~/github-sandbox/cebartling/kafka-experiments && pwd && git-set-ssh-key && git-set-config"
-alias kafka-experiments-example1-cd="cd ~/github-sandbox/cebartling/kafka-experiments/example1 && pwd && git-set-ssh-key && git-set-config"
-alias kafka-experiments-example2-cd="cd ~/github-sandbox/cebartling/kafka-experiments/example2 && pwd && git-set-ssh-key && git-set-config"
-alias kafka-experiments-example3-cd="cd ~/github-sandbox/cebartling/kafka-experiments/example3 && pwd && git-set-ssh-key && git-set-config"
-alias kafka-experiments-example4-cd="cd ~/github-sandbox/cebartling/kafka-experiments/example4 && pwd && git-set-ssh-key && git-set-config"
+alias kafka-experiments-experiment-01-cd="cd ~/github-sandbox/cebartling/kafka-experiments/experiment-01 && pwd && git-set-ssh-key && git-set-config"
+alias kafka-experiments-experiment-02-cd="cd ~/github-sandbox/cebartling/kafka-experiments/experiment-02 && pwd && git-set-ssh-key && git-set-config"
+alias kafka-experiments-experiment-03-cd="cd ~/github-sandbox/cebartling/kafka-experiments/experiment-03 && pwd && git-set-ssh-key && git-set-config"
+alias kafka-experiments-experiment-04-cd="cd ~/github-sandbox/cebartling/kafka-experiments/experiment-04 && pwd && git-set-ssh-key && git-set-config"
 
 
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
 echo $fg[yellow]kafka-experiments-cd$reset_color
-echo $fg[yellow]kafka-experiments-example1-cd$reset_color
-echo $fg[yellow]kafka-experiments-example2-cd$reset_color
-echo $fg[yellow]kafka-experiments-example3-cd$reset_color
-echo $fg[yellow]kafka-experiments-example4-cd$reset_color
+echo $fg[yellow]kafka-experiments-experiment-01-cd$reset_color
+echo $fg[yellow]kafka-experiments-experiment-02-cd$reset_color
+echo $fg[yellow]kafka-experiments-experiment-03-cd$reset_color
+echo $fg[yellow]kafka-experiments-experiment-04-cd$reset_color
 echo $fg[cyan]---------------------------------------------$reset_color
