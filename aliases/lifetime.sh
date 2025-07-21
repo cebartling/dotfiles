383e0e0d616462b03bfe6233ba84ce7f1bb557cb
Christopher Bartling <chris.bartling@gmail.com>
2025-07-21 09:46:15 -0500

    feat: adding alias

--- aliases/lifetime.sh
+++ aliases/lifetime.sh
@@ -9,6 +9,8 @@ alias lt-github-home-cd="cd ~/github-sandbox/lifetime && pwd && lt-git-setup"
 alias lt-splunkspike-cd="cd ~/github-sandbox/lifetime/splunkspike && pwd && lt-git-setup"
 alias lt-ops-confluent-kafka-cd="cd ~/github-sandbox/lifetime/ops-confluent-kafka && pwd && lt-git-setup"
 alias lt-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/credit-card-account-consumer && pwd && lt-git-setup"
+alias lt-suite-creditcardaccount-debezium-connector-cd="cd ~/github-sandbox/lifetime/ltsuite-creditcardaccount-debezium-connector && pwd && lt-git-setup"
+
 
 
 alias lt-add-ssh-key="ssh-add -D && ssh-add -t 8h ~/.ssh/id_ed25519_life_time && ssh-add -l"
@@ -21,6 +23,7 @@ echo $fg[yellow]lt-github-home-cd$reset_color
 echo $fg[yellow]lt-splunkspike-cd$reset_color
 echo $fg[yellow]lt-ops-confluent-kafka-cd$reset_color
 echo $fg[yellow]lt-credit-card-account-consumer-cd$reset_color
+echo $fg[yellow]lt-suite-creditcardaccount-debezium-connector-cd$reset_color
 echo ""
 echo $fg[yellow]lt-add-ssh-key$reset_color
 echo $fg[yellow]lt-git-config$reset_color
