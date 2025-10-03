836f386a05edc6a861ad47b9baed6b779083fc3b
Christopher Bartling <chris.bartling@gmail.com>
2025-10-03 12:11:21 -0500

    feat: adding Life Time aliases

--- aliases/lifetime.sh
+++ aliases/lifetime.sh
@@ -6,32 +6,38 @@ colors
 echo ""
 echo $fg[cyan]Configuring Life Time, Inc. aliases...$reset_color
 
+alias lt-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/credit-card-account-consumer && pwd && lt-git-setup"
+alias lt-register-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/register-credit-card-account-consumer && pwd && lt-git-setup"
+alias lt-chase-commerce-gateway-api-cd="cd ~/github-sandbox/lifetime/chase-commerce-gateway-api && pwd && lt-git-setup"
+
 alias lt-github-home-cd="cd ~/github-sandbox/lifetime && pwd && lt-git-setup"
 alias lt-splunkspike-cd="cd ~/github-sandbox/lifetime/splunkspike && pwd && lt-git-setup"
 alias lt-ops-confluent-kafka-cd="cd ~/github-sandbox/lifetime/ops-confluent-kafka && pwd && lt-git-setup"
-alias lt-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/credit-card-account-consumer && pwd && lt-git-setup"
-alias lt-register-credit-card-account-consumer-cd="cd ~/github-sandbox/lifetime/register-credit-card-account-consumer && pwd && lt-git-setup"
 alias lt-suite-creditcardaccount-debezium-connector-cd="cd ~/github-sandbox/lifetime/ltsuite-creditcardaccount-debezium-connector && pwd && lt-git-setup"
 alias lt-chase-oauth-spike-cd="cd ~/github-sandbox/lifetime/chase-oauth-spike && pwd && lt-git-setup"
 
 alias lt-docker-compose-down="docker compose --profile infrastructure down -v --remove-orphans"
 
 alias lt-add-ssh-key="ssh-add -D && ssh-add -t 24h ~/.ssh/id_ed25519 && ssh-add -l"
-alias lt-add-personal-ssh-key="ssh-add -D && ssh-add -t 24h ~/.ssh/id_ed25519_personal && ssh-add -l"
+alias lt-add-personal-ssh-key="ssh-add -D && ssh-add -t 24h ~/.ssh/id_ed25519_personal && ssh-add   -l"
 alias lt-git-config="git config user.name \"Christopher Bartling\" && git config user.email \"cbartling@lt.life\""
 alias lt-git-setup="lt-add-ssh-key && lt-git-config"
+alias lt-git-personal-setup="lt-add-personal-ssh-key && lt-git-config"
 
 echo ""
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
 echo $fg[yellow]lt-github-home-cd$reset_color
+echo ""
 echo $fg[yellow]lt-credit-card-account-consumer-cd$reset_color
 echo $fg[yellow]lt-register-credit-card-account-consumer-cd$reset_color
+echo $fg[yellow]lt-chase-commerce-gateway-api-cd$reset_color
 echo ""
 echo $fg[yellow]lt-add-ssh-key$reset_color
 echo $fg[yellow]lt-add-personal-ssh-key$reset_color
 echo $fg[yellow]lt-git-config$reset_color
 echo $fg[yellow]lt-git-setup$reset_color
+echo $fg[yellow]lt-git-personal-setup$reset_color
 echo $fg[yellow]lt-docker-compose-down$reset_color
 echo ""
 echo $fg[yellow]lt-splunkspike-cd$reset_color
