5d423fd9da63d0331f330a4e58c408a0840dfc28
Christopher Bartling <chris.bartling@gmail.com>
2025-11-21 12:46:25 -0600

    feat: more aliases for spring-boot-spikes

--- aliases/spring-boot-spikes.sh
+++ aliases/spring-boot-spikes.sh
@@ -10,6 +10,7 @@ colors
 echo ""
 echo $fg[cyan]Configuring aliases for Spring Boot spikes...$reset_color
 
+alias spring-boot-spikes-git-clone="cd ~/github-sandbox/cebartling && git clone git@github.com:cebartling/spring-boot-spikes.git"
 alias spring-boot-spikes-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes && sdk use java 21.0.8-amzn"
 alias spring-boot-spike-vault-integration-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/vault-integration && sdk use java 21.0.8-amzn"
 alias spring-boot-spike-resiliency-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/resiliency-spike && sdk use java 21.0.8-amzn"
@@ -18,6 +19,8 @@ alias spring-boot-spike-resiliency-spike-cd="cd ~/github-sandbox/cebartling/spri
 echo ""
 echo $fg[cyan]Available aliases$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
+echo $fg[yellow]spring-boot-spikes-git-clone$reset_color
+echo ""
 echo $fg[yellow]spring-boot-spikes-cd$reset_color
 echo ""
 echo $fg[yellow]spring-boot-spike-vault-integration-cd$reset_color
