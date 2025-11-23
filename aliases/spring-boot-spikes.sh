b973ce8498198216c0b856ebc2ee2c5ffda815bd
Christopher Bartling <chris.bartling@gmail.com>
2025-11-22 21:49:56 -0600

    fix: tweaking sdk env usage

--- aliases/spring-boot-spikes.sh
+++ aliases/spring-boot-spikes.sh
@@ -12,8 +12,8 @@ echo $fg[cyan]Configuring aliases for Spring Boot spikes...$reset_color
 
 alias spring-boot-spikes-git-clone="cd ~/github-sandbox/cebartling && git clone git@github.com:cebartling/spring-boot-spikes.git"
 alias spring-boot-spikes-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes && sdk use java 21.0.8-amzn"
-alias spring-boot-spike-cqrs-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/cqrs-spike && sdk use"
-alias spring-boot-spike-resiliency-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/resiliency-spike && sdk use java 21.0.8-amzn"
+alias spring-boot-spike-cqrs-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/cqrs-spike && sdk env"
+alias spring-boot-spike-resiliency-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/resiliency-spike && sdk env"
 
 
 echo ""
