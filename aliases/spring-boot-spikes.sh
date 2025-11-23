64397dd3117a6f1b967d52ac865ef07ff068f295
Christopher Bartling <chris.bartling@gmail.com>
2025-11-22 21:47:07 -0600

    feat: adding alias for spring-boot-spikes

--- aliases/spring-boot-spikes.sh
+++ aliases/spring-boot-spikes.sh
@@ -12,7 +12,7 @@ echo $fg[cyan]Configuring aliases for Spring Boot spikes...$reset_color
 
 alias spring-boot-spikes-git-clone="cd ~/github-sandbox/cebartling && git clone git@github.com:cebartling/spring-boot-spikes.git"
 alias spring-boot-spikes-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes && sdk use java 21.0.8-amzn"
-alias spring-boot-spike-vault-integration-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/vault-integration && sdk use java 21.0.8-amzn"
+alias spring-boot-spike-cqrs-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/cqrs-spike && sdk use"
 alias spring-boot-spike-resiliency-spike-cd="cd ~/github-sandbox/cebartling/spring-boot-spikes/resiliency-spike && sdk use java 21.0.8-amzn"
 
 
@@ -23,6 +23,6 @@ echo $fg[yellow]spring-boot-spikes-git-clone$reset_color
 echo ""
 echo $fg[yellow]spring-boot-spikes-cd$reset_color
 echo ""
-echo $fg[yellow]spring-boot-spike-vault-integration-cd$reset_color
+echo $fg[yellow]spring-boot-spike-cqrs-spike-cd$reset_color
 echo $fg[yellow]spring-boot-spike-resiliency-spike-cd$reset_color
 echo $fg[cyan]---------------------------------------------------$reset_color
