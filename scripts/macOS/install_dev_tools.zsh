278c84e50b0ebb7e43fd63bc5bbee947d95fb874
Christopher Bartling <chris.bartling@gmail.com>
2026-02-22 22:55:40 -0600

    feat: adding kaf to macOS dev tools install

--- scripts/macOS/install_dev_tools.zsh
+++ scripts/macOS/install_dev_tools.zsh
@@ -8,6 +8,7 @@ echo $fg[cyan]Installing development tools on macOS...$reset_color
 brew update
 
 brew install kcat
+brew install kaf
 brew install avro-tools
 
 echo $fg[cyan]Finished installing development tools on macOS!$reset_color
