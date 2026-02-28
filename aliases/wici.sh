e39fb53ff22da210e1ca419c8d1211e74838eb55
Christopher Bartling <chris.bartling@gmail.com>
2026-02-28 08:29:47 -0600

    feat: new alias

--- aliases/wici.sh
+++ aliases/wici.sh
@@ -7,8 +7,9 @@ echo ""
 echo $fg[cyan]Configuring Schoolhouse Educational Services aliases...$reset_color
 
 alias wici-home-cd="cd ~/github-sandbox/schoolhouse-educational-services && pwd && wici-git-setup"
-alias wici-api-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-api && pwd && wici-git-setup"
-alias wici-client-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-client && pwd && wici-git-setup"
+alias wici-api-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-api && pwd && wici-git-setup && nvm use"
+alias wici-client-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-client && pwd && wici-git-setup && nvm use"
+alias wici-acceptance-tests-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-acceptance-tests && pwd && wici-git-setup && nvm use"
 
 alias wici-services-up="docker compose up -d"
 alias wici-services-down="docker compose down -v --remove-orphans"
@@ -24,6 +25,7 @@ echo $fg_bold[yellow]wici-home-cd$reset_color
 echo ""
 echo $fg_bold[yellow]wici-api-cd$reset_color
 echo $fg_bold[yellow]wici-client-cd$reset_color
+echo $fg_bold[yellow]wici-acceptance-tests-cd$reset_color
 echo ""
 echo $fg_bold[yellow]wici-add-ssh-key$reset_color
 echo $fg_bold[yellow]wici-git-config$reset_color
