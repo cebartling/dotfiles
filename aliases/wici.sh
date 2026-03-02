b4d08ed0194f3a804d66ec13fbccaed8d8758993
Christopher Bartling <chris.bartling@gmail.com>
2026-03-01 21:30:27 -0600

    feat: adding wici-tools-cd alias

--- aliases/wici.sh
+++ aliases/wici.sh
@@ -9,6 +9,7 @@ echo $fg[cyan]Configuring Schoolhouse Educational Services aliases...$reset_colo
 alias wici-home-cd="cd ~/github-sandbox/schoolhouse-educational-services && pwd && wici-git-setup"
 alias wici-api-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-api && pwd && wici-git-setup && nvm use"
 alias wici-client-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-client && pwd && wici-git-setup && nvm use"
+alias wici-tools-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-tools && pwd && wici-git-setup"
 alias wici-acceptance-tests-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-acceptance-tests && pwd && wici-git-setup && nvm use"
 
 alias wici-services-up="docker compose up -d"
@@ -25,6 +26,7 @@ echo $fg_bold[yellow]wici-home-cd$reset_color
 echo ""
 echo $fg_bold[yellow]wici-api-cd$reset_color
 echo $fg_bold[yellow]wici-client-cd$reset_color
+echo $fg_bold[yellow]wici-tools-cd$reset_color
 echo $fg_bold[yellow]wici-acceptance-tests-cd$reset_color
 echo ""
 echo $fg_bold[yellow]wici-add-ssh-key$reset_color
