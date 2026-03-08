7a1dec0dc3eaa16881b3e5fb22f57c54b974f683
Christopher Bartling <chris.bartling@gmail.com>
2026-03-08 11:06:21 -0500

    feat: adding new alias for WICI

--- aliases/wici.sh
+++ aliases/wici.sh
@@ -11,6 +11,7 @@ alias wici-api-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-api
 alias wici-client-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-client && pwd && wici-git-setup && nvm use"
 alias wici-tools-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-tools && pwd && wici-git-setup"
 alias wici-acceptance-tests-cd="cd ~/github-sandbox/schoolhouse-educational-services/wici-acceptance-tests && pwd && wici-git-setup && nvm use"
+alias wici-web-client-v2-cd="cd ~/github-sandbox/schoolhouse-educational-services/web-client-v2 && pwd && wici-git-setup && nvm use"
 
 alias wici-services-up="docker compose up -d"
 alias wici-services-down="docker compose down -v --remove-orphans"
