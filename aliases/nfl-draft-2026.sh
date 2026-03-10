181ea288668d8da0be1d4416e8d9ab45ef3eaa90
Christopher Bartling <chris.bartling@gmail.com>
2026-03-10 07:48:02 -0500

    feat: adding nfl-draft-2026 aliases

---
+++ aliases/nfl-draft-2026.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+autoload colors
+colors
+
+echo ""
+echo $fg[cyan]Configuring NFL Draft 2026 aliases...$reset_color
+
+alias nfl-draft-2026-cd="cd ~/github-sandbox/cebartling/nfl-draft-2026/ && pwd && personal-git-setup"
+
+
+echo ""
+echo $fg[cyan]Available aliases$reset_color
+echo $fg[cyan]---------------------------------------------------$reset_color
+echo $fg_bold[yellow]nfl-draft-2026-cd$reset_color
+echo $fg[cyan]---------------------------------------------------$reset_color
