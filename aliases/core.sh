f44a4973caeaa61359132e73ee7cecc6112d3369
Christopher Bartling <chris.bartling@gmail.com>
2026-04-03 09:09:57 -0500

    feat: creating new daily-update alias

--- aliases/core.sh
+++ aliases/core.sh
@@ -11,7 +11,7 @@ alias cat=bat --paging=never
 #alias ll='eza -l --icons --no-user --group-directories-first  --time-style long-iso'
 alias ls='eza -l --group-directories-first --color=auto --icons --no-permissions --no-user'
 alias ll='eza -lahF --group-directories-first --color=auto --icons'
-alias homebrew-update="brew update && brew upgrade && brew cleanup"
+alias daily-update="brew update && brew upgrade && brew cleanup && claude update"
 
 alias zshconfig="code ~/.zshrc"
 alias aliases-config="code ~/.dotfiles/aliases/core.sh"
